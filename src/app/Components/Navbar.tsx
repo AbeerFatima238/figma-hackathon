da76e9413f675790f380ab156a13ff1eb6baaec0
AbeerFatima238 <abeerfatimahom2@gmail.com>
2024-12-17 01:03:45 +0500

    first commit

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -11,41 +11,22 @@ const Navbar = () => {
   return (
     <div>
       {/* Apply the dynamic background color based on whether it's the homepage */}
-      <div className={`w-full h-[150px] ${isHomePage ? 'bg-[#FBEBB5]' : 'bg-white'} shadow-lg`}>
-        {/* Navbar Container with slight left shift */}
-        <div className="w-full h-full flex items-center justify-between px-8 lg:px-16 ">
-          {/* Left Navigation Items */}
-          <ul className="hidden lg:flex gap-10 text-center ml-64">
-            <li><Link href="/" className="text-xl font-medium font-Poppins hover:underline">Home</Link></li>
-            <li><Link href="/Shop" className="text-xl font-medium font-Poppins hover:underline">Shop</Link></li>
-            <li><Link href="/Blog" className="text-xl font-medium font-Poppins hover:underline">About</Link></li>
-            <li><Link href="/Contact" className="text-xl font-medium font-Poppins hover:underline">Contact</Link></li>
-          </ul>
-
-          {/* Right Icon Section */}
-          <ul className="hidden lg:flex gap-14 items-center">
-            <Link href="/Account">
-              <li><User className="text-xl" /></li>
-            </Link>
-            <li><Search className="text-xl" /></li>
-            <li><Heart className="text-xl" /></li>
-            <li><ShoppingCart className="text-xl" /></li>
-          </ul>
-
-          {/* Mobile Navigation (visible on smaller screens) */}
-          <ul className="lg:hidden flex gap-4 items-center">
-            <li><Link href="/Account"><User className="text-xl" /></Link></li>
-            <li><Search className="text-xl" /></li>
-            <li><Heart className="text-xl" /></li>
-            <li><ShoppingCart className="text-xl" /></li>
-          </ul>
-
-          {/* Mobile Menu Toggle (visible on smaller screens) */}
-          <div className="lg:hidden flex items-center">
-            <button className="text-2xl text-black">
-              {/* Mobile menu icon (can add a hamburger menu here for toggling mobile view) */}
-              <span className="material-icons">menu</span>
-            </button>
+      <div className={`w-[1440px] h-[150px] ${isHomePage ? 'bg-[#FBEBB5]' : 'bg-white'} shadow-lg`}>
+        {/* Add shadow-lg class for a large shadow */}
+        <div className='w-[430px] h-2 top-[38px] left-[505px] mx-96'>
+          <div className='w-12 h-2 top-[38px] pt-8 left-[505]'>
+            <ul className='flex text-center pt-8 gap-10'>
+              <li><Link href="/" className='text-xl font-medium font-Poppins hover:underline'>Home</Link></li>
+              <li><Link href="/Shop" className='text-xl font-medium font-Poppins hover:underline'>Shop</Link></li>
+              <li><Link href="/Blog" className='text-xl font-medium font-Poppins hover:underline'>About</Link></li>
+              <li><Link href="/Contact" className='text-xl font-medium font-Poppins hover:underline'>Contact</Link></li>
+            </ul>
+            <ul className="flex items-center ml-[600px] mt-[-28px] gap-14">
+              <Link href="/Account"><li><User /></li></Link>
+              <li><Search /></li>
+              <li><Heart /></li>
+              <li><ShoppingCart /></li>
+            </ul>
           </div>
         </div>
       </div>
